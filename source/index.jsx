c0e98845434dfc742a59e783525ec862856ee915
xcatliu <xcatliu@gmail.com>
2015-07-04 08:48:42 +0800

    Update index.jsx

---
+++ source/index.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Index = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Use React to generate your awesome static website</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = Index;
